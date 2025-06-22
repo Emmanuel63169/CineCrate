1ef2e4fa87072e8056a41d3827892bbcbeb9c623
Emmanuel63169 <emmanuel63169@gmail.com>
2025-06-21 19:56:52 -0600

    Get/api/movies router created

--- server/api.js
+++ server/api.js
@@ -11,10 +11,24 @@ const {
     createMovie,
     authenticateUser,
     getUserById,
+    getMovies,
 } = require('./db')
 
-function verifyToken(req, res, next) {
+// function verifyToken(req, res, next) {
 
-}
+// }
+
+// GET/api/movies
+router.get('/movies', async(req, resizeBy, next) => {
+    try{
+        const response = await getMovies();
+        if(response.length === 0) {
+            return resizeBy.status(400).json({message: 'No Movies?'})
+        }
+        resizeBy.status(200).send(response)
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = router;
