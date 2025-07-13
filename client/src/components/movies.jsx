aea867a5e81af05492fb8040b33c8137465389c9
Emmanuel63169 <emmanuel63169@gmail.com>
2025-07-12 19:36:14 -0600

    added fetch for movies from MOVIES_API_URL

--- client/src/components/movies.jsx
+++ client/src/components/movies.jsx
@@ -1,9 +1,26 @@
 // Movies Page - where all movies can be browsed
-
+import { useEffect } from "react";
 
 import './CSS/movies.css'
 
-export default function moviesPage() {
+export default function moviesPage({ movies, setMovies}) {
+const MOVIES_API_URL = "http://localhost:3000/api/movies";
+
+useEffect(() => { 
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(MOVIES_API_URL);
+      const data = await response.json();
+      console.log('Movies Found!', data)
+      setMovies(data)
+    } catch (error) {
+      console.error('Error Fetching the Movies :(', error);
+    }
+  }
+
+  fetchMovies();
+}, []);
 
     return (
       <>
