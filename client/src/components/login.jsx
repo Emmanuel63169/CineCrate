01be74d3bd0e2fea0ec678bc550512513dcfec4a
Emmanuel63169 <emmanuel63169@gmail.com>
2025-08-01 18:45:14 -0600

    coded login page inputs

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,16 +1,30 @@
 // Login Page - where people sign in to their accounts
 
-
+import {useState} from 'react'
 import './CSS/login.css'
 
 export default function loginPage() {
 
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      console.log('Username =>', username, 'Password =>', password)
+    }
     return (
       <>
         <div className='loginPage'>
             <h1>Login Page</h1>
             <div className='loginInputs'>
-                <p>where'd the inputs go?</p>
+                <form onSubmit={handleSubmit}>
+                  <div className='formGroup'>
+                    Username: <input value={username} onChange={(e) => setUsername(e.target.value)} required/>
+                    Password: <input value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                  </div>
+                  <button>Submit</button>
+                </form>
             </div>
         </div>  
       </>
