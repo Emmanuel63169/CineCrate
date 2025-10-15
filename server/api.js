87345ef7c9f58ae1cba2d365b3a0775753737f7d
Emmanuel63169 <emmanuel63169@gmail.com>
2025-10-14 22:07:01 -0600

    users table/router setup and token verification

--- server/api.js
+++ server/api.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const router = express.Router()
+const router = express.Router();
 
 const {
     pool,
@@ -15,9 +16,24 @@ const {
     getMoviesWithGenres,
 } = require('./db')
 
-// function verifyToken(req, res, next) {
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
 
-// }
+    if (!authHeader) {
+        return res.status(403).json({ message: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Invalid or expired token' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
 
 // GET/api/movies
 router.get('/movies', async(req, res, next) => {
@@ -55,5 +71,59 @@ router.get('/movies/:id', async(req, res, next) => {
         next(error)
     }
 })
+
+//  POST /api/users/register
+router.post('/users/register', async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await pool.query(
+            `INSERT INTO users (username, email, password_hash)
+            VALUES ($1, $2, $3)
+            RETURNING user_id, username, email;`,
+            [username, email, hashedPassword]
+        );
+
+        const user = result.rows[0];
+        res.status(201).json(user);
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.post('/users/login', async (req, res, next) => {
+    const {email, password } = req.body;
+
+    try {
+        const result = await pool.query(
+            'SELECT * FROM users WHERE email = $1;',
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const user = result.rows[0];
+        const isValid = await bcrypt.compare(password, user.password_hash);
+
+        if (!isValid) {
+            return res.status(401).json({ message: 'Invalid Credentials' });
+        }
+
+        // JWT creation
+        const token = jwt.sign(
+            {user_id: user.user_id, username: user.username, is_admin: user.is_admin },
+            process.env.JWT_SECRET,
+            { expiresIn: '2h' }
+        );
+
+        res.status(200).json({ token });
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
